78c4ba1e6ae2ad63a76646e4af7c69556522c002
Nuradil Zhanadil uulu <nuradilzhanadiluulu21@gmail.com>
2024-09-15 12:30:57 -0500

    first commit

---
+++ src/test/java/com/houstondev/HoustonHotel/HoustonHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.houstondev.HoustonHotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoustonHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
